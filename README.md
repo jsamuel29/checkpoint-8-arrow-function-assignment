c9a6a8784b2898ab2d0da94ba7b1a2f11f18cc6e
Jonathan Samuel <jonathansamuel83@gmail.com>
2022-07-05 13:41:27 -0500

    assignments

---
+++ README.md
@@ -0,0 +1,4 @@
+# checkpoint 8 arrow function assignment
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/jsamuel1/pen/LYdpoWp](https://codepen.io/jsamuel1/pen/LYdpoWp).
+
---
+++ src/script.js
@@ -0,0 +1,10 @@
+const greeting = () => {
+  const salute = 'Hello World';
+  if (salute === 'Hello World') {
+    return 'Hello World';
+  } else {
+    return 'wrong answer';
+  }
+};
+
+console.log(greeting());
